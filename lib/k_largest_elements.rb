43f22169659290dd9a5f42d214e3a0f9c415df33
FelixAlvarado <felixalvarado653@gmail.com>
2018-07-05 10:43:00 -0700

    finished largest k elements

--- lib/k_largest_elements.rb
+++ lib/k_largest_elements.rb
@@ -1,5 +1,15 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
-
+    length = array.length
+    array2 = array.dup
+    length.times do |i|
+        BinaryMinHeap.heapify_down(array2, i, length)
+    end 
+    heap = BinaryMinHeap.new(array2)
+    (length).times do
+      array.shift
+      array.push(heap.extract)
+    end 
+    array.drop(length - k)
 end
